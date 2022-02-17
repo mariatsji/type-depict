72394a5e4f4b07c958099a30101b6d8e1fe9fed4
Sjur Millidahl <smillida@cisco.com>
2022-02-17 14:29:08 +0100

    perfect

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,30 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
 import Buffer (Buffer)
 import qualified Buffer
+import Control.Monad (join)
+import Data.Char (isNumber)
 import Data.Foldable (fold, traverse_)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment (getArgs)
-import Control.Monad (join)
 
-data Command = Search Text | Persist Text | DumpBuffer | Help deriving stock (Eq, Show)
+data Command = Search Text | Persist Text | DumpBuffer | Help | Todo deriving stock (Eq, Show)
 
 snippets :: FilePath
 snippets = "/Users/sjurmillidahl/.hell/snippets"
 
+todoF :: FilePath
+todoF = "/Users/sjurmillidahl/.hell/todo"
+
 main :: IO ()
 main = do
-    print "h(askell) (sh)ell"
+    _print "h(askell) (sh)ell"
     args <- getArgs
     snippetlines <- readFileAsLines snippets
     shelllines <- readFilesAsLines ["/Users/sjurmillidahl/.zsh_history", "/Users/sjurmillidahl/.bash_history"]
@@ -31,23 +36,53 @@ main = do
             traverse_ (\v -> print (t <> " " <> v)) (Buffer.get t buffer)
         Persist t -> do
             TIO.writeFile snippets (T.unlines (t : snippetlines))
-            print $ "stored [" <> t <> "]"
+            _print $ "stored [" <> t <> "]"
         Help -> do
-            print "usage:"
-            print "persist: `hell p some kind of snippet`"
-            print "search: `hell s searchword"
-            print "dump: `hell d`"
+            _print "usage:"
+            _print "persist: `hell p some kind of snippet`"
+            _print "search: `hell s searchword"
+            _print "dump: `hell d`"
+        Todo -> todo
+
+todo :: IO ()
+todo = do
+    oldlines <- readFileAsLines todoF
+    let presentedLines = presentLines oldlines
+    traverse_ print presentedLines
+    _print "todo (q to exit)"
+    input <- TIO.getLine
+    if input == "q"
+        then pure ()
+        else do
+            let newlines =
+                    if all isNumber (T.unpack input)
+                        then
+                            let i = read @Int (T.unpack input)
+                             in unpresentLines $ filter (\(j, _) -> j /= i) presentedLines
+                        else oldlines <> [input]
+            TIO.writeFile todoF (T.unlines newlines)
+            todo
+
+_print :: Text -> IO ()
+_print = putStrLn . T.unpack
+
+presentLines :: [a] -> [(Int, a)]
+presentLines = zip [0 :: Int ..]
+
+unpresentLines :: [(Int, a)] -> [a]
+unpresentLines = fmap snd
 
 parseCmd :: [String] -> Command
 parseCmd [] = Help
-parseCmd (c:l) = case c of
+parseCmd (c : l) = case c of
     "p" -> Persist (mkLine l)
     "s" -> Search (mkLine l)
     "d" -> DumpBuffer
+    "todo" -> Todo
     _ -> Help
 
 mkLine :: [String] -> Text
-mkLine s = T.strip . T.intercalate " " $ T.pack <$> s 
+mkLine s = T.strip . T.intercalate " " $ T.pack <$> s
 
 readFilesAsLines :: [FilePath] -> IO [Text]
 readFilesAsLines l = join <$> traverse readFileAsLines l
