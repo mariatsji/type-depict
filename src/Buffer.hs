1a34131bda922da8ba964ca746bded75c28d7f9e
Sjur Millidahl <smillida@cisco.com>
2022-02-20 17:40:02 +0100

    compaction

--- src/Buffer.hs
+++ src/Buffer.hs
@@ -2,6 +2,7 @@ module Buffer (Buffer (..), empty, fromList, get, dump) where
 
 import Data.MultiMap (MultiMap)
 import qualified Data.MultiMap as MM
+import qualified Data.Set as Set
 import Data.Text (Text)
 import qualified Data.Text as T
 
@@ -11,8 +12,12 @@ empty :: Buffer
 empty = Buffer MM.empty
 
 fromList :: [(Text, Text)] -> Buffer
-fromList keyvals = Buffer (MM.fromList (filter nonempty keyvals))
+fromList keyvals = compact $ Buffer (MM.fromList (filter nonempty keyvals))
     where nonempty (k,v) = not $ T.null k || T.null v
+
+compact :: Buffer -> Buffer
+{-# INLINE compact #-}
+compact (Buffer mm) = Buffer $ MM.fromMap $ Set.toList <$> MM.toMapOfSets mm
 
 get :: Text -> Buffer -> [Text]
 get k (Buffer mm) = MM.lookup k mm
