a93aca8b79fcc0834625cde0723f821f94725106
Sjur Millidahl <smillida@cisco.com>
2022-03-02 11:55:17 +0100

    bubbles not boxes

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,8 @@ import Visual
 
 main :: IO ()
 main = do
-    let visual = Connect (Dot "a") (Embellish (Dot "b"))
+    let visual = Embellish $ Connect (Embellish (Dot "a")) (Embellish (Dot "b"))
         container = [Version_ <<- "1.1", Width_ <<- "600", Height_ <<- "300"]
-        box = Box{w = 600, h = 300, x = 0, y = 0}
-        svg = doctype <> with (svg11_ (renderSvg box visual)) container
+        bubble = Bubble{cx = 300, cy = 150, r = 150}
+        svg = doctype <> with (svg11_ (renderSvg bubble visual)) container
     renderToFile "index.html" svg
--- src/Visual.hs
+++ src/Visual.hs
@@ -24,32 +24,31 @@ render = \case
 instance Show Visual where
     show = T.unpack . render
 
-data Box = Box
-    { w :: Float -- my width
-    , h :: Float -- my height
-    , x :: Float -- offset x in container
-    , y :: Float -- offset y in container
+data Bubble = Bubble
+    { cx :: Float -- how far along the total axis is this bubble
+    , cy :: Float -- how high up is this bubble bubble
+    , r :: Float -- radius of this bubble
     }
 
-renderSvg :: Box -> Visual -> Element
-renderSvg box@Box{..} = \case
+renderSvg :: Bubble -> Visual -> Element
+renderSvg bubble@Bubble{..} = \case
     Dot _ ->
-        let cx = x + w / 2
-            cy = y + h / 2
-            cT = T.pack . show
+        let cT = T.pack . show
          in circle_ [Cx_ <<- cT cx, Cy_ <<- cT cy, R_ <<- "4", Fill_ <<- "black"]
     Connect a b ->
-        let boxA = Box{w = w / 2, h = h, x = x, y = y}
-            boxB = Box{w = w / 2, h = h, x = w / 2, y = y}
-            path = path_ [D_ <<- centre mA boxA <> centre lA boxB, Stroke_ <<- "black"]
-         in renderSvg boxA a <> path <> renderSvg boxB b
+        let (bubbleA, bubbleB) = split bubble
+            path = path_ [D_ <<- centre mA bubbleA <> centre lA bubbleB, Stroke_ <<- "black"]
+         in renderSvg bubbleA a <> path <> renderSvg bubbleB b
     Embellish a ->
-        let boxA = Box{w = w - 50, h = h - 50, x = x + 25, y = y + 25}
-            cx = x + w / 2
-            cy = y + h / 2
+        let bubbleA = Bubble{cx = cx, cy = cy, r = r - 10}
             cT = T.pack . show
-        in circle_ [Cx_ <<- cT cx, Cy_ <<- cT cy, R_ <<- "100", Stroke_ <<- "black", Stroke_width_ <<- "3", Fill_ <<- "none"] <> renderSvg boxA a
+         in circle_ [Cx_ <<- cT cx, Cy_ <<- cT cy, R_ <<- T.pack (show r), Stroke_ <<- "black", Stroke_width_ <<- "3", Fill_ <<- "none"] <> renderSvg bubbleA a
   where
-    centre :: (Float -> Float -> Text) -> Box -> Text
-    centre svgOp Box{..} = svgOp (x + (w / 2)) (y + (h / 2))
+    centre :: (Float -> Float -> Text) -> Bubble -> Text
+    centre svgOp Bubble{..} = svgOp cx cy
 
+    split :: Bubble -> (Bubble, Bubble)
+    split Bubble{..} =
+        let cxA = cx - r + (r / 3)
+            cxB = cx + r - (r / 3)
+         in (Bubble{cx = cxA, cy = cy, r = r / 3}, Bubble{cx = cxB, cy = cy, r = r / 3})
