f9a115d0fb638fa56cbe60bbd6826ba394d7633f
Sjur Millidahl <smillida@cisco.com>
2022-03-02 09:08:35 +0100

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import Visual
 
 main :: IO ()
 main = do
-    let visual = Connect (Dot "a") (Dot "b")
+    let visual = Connect (Dot "a") (Embellish (Dot "b"))
         container = [Version_ <<- "1.1", Width_ <<- "600", Height_ <<- "300"]
         box = Box{w = 600, h = 300, x = 0, y = 0}
         svg = doctype <> with (svg11_ (renderSvg box visual)) container
--- src/Visual.hs
+++ src/Visual.hs
@@ -35,15 +35,20 @@ renderSvg :: Box -> Visual -> Element
 renderSvg box@Box{..} = \case
     Dot _ ->
         let cx = x + w / 2
-            cxT = T.pack . show $ cx
             cy = y + h / 2
-            cyT = T.pack . show $ cy
-         in circle_ [Cx_ <<- cxT, Cy_ <<- cyT, R_ <<- "4", Fill_ <<- "black"]
+            cT = T.pack . show
+         in circle_ [Cx_ <<- cT cx, Cy_ <<- cT cy, R_ <<- "4", Fill_ <<- "black"]
     Connect a b ->
         let boxA = Box{w = w / 2, h = h, x = x, y = y}
             boxB = Box{w = w / 2, h = h, x = w / 2, y = y}
             path = path_ [D_ <<- centre mA boxA <> centre lA boxB, Stroke_ <<- "black"]
          in renderSvg boxA a <> path <> renderSvg boxB b
+    Embellish a ->
+        let boxA = Box{w = w - 50, h = h - 50, x = x + 25, y = y + 25}
+            cx = x + w / 2
+            cy = y + h / 2
+            cT = T.pack . show
+        in circle_ [Cx_ <<- cT cx, Cy_ <<- cT cy, R_ <<- "100", Stroke_ <<- "black", Stroke_width_ <<- "3", Fill_ <<- "none"] <> renderSvg boxA a
   where
     centre :: (Float -> Float -> Text) -> Box -> Text
     centre svgOp Box{..} = svgOp (x + (w / 2)) (y + (h / 2))
